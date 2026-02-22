e1ac7d0b7a3c137788ce7aa616ea8f4d7136e867
AnimeshLPU <tripathianimesh38@gmail.com>
2026-02-22 20:43:27 +0530

    updated readme

--- Backend/src/routes/submissionRoutes.js
+++ Backend/src/routes/submissionRoutes.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const codeSubmitRouter = express.Router();
-<<<<<<< HEAD
-const { submitCode, runCode, getSubmissions } = require('../controllers/submissionController');
-=======
 const { submitCode, runCode } = require('../controllers/submissionController');
->>>>>>> 7b7a4e10a74f2c78a63df608b24ef7c1a39337f1
 const userMiddleware = require('../middleware/userMiddleware');
 
 codeSubmitRouter.post('/submit/:problemId', userMiddleware, submitCode);
