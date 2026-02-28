a02c9529cb16340ef5b843a9fcbb7bffbe9b304e
Animesh Tripathi <tripathianimesh38@gmail.com>
2026-02-28 13:47:29 +0530

    Update axiosClient baseURL to production URL

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-    baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:5000",
+    baseURL: import.meta.env.VITE_BACKEND_URL || "https://codeprep-1kzd.onrender.com",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json",
@@ -20,6 +20,7 @@ axiosClient.interceptors.request.use(config => {
     }
     return config;
 });
+
 
 
 
